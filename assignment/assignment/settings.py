a89ed3fdf8883f5ebf58a213bbea69f204eb903a
giriudogere@gmail.com <Girish.Udogere.Ramachandrappa@evryindia.in>
2021-05-26 14:20:12 +0530

    cfg

--- assignment/assignment/settings.py
+++ assignment/assignment/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'assignment.assignment.backend',
+    'backend',
     'import_export',
     'rest_framework',
 ]
